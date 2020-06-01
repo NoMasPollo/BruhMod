71eee756df3baba2e946e755f61f10b3ccc05515
NoMasPollo <65381377+NoMasPollo@users.noreply.github.com>
2020-05-31 22:45:39 -0700

    Create customeffect.js

---
+++ scripts/customeffect.js
@@ -0,0 +1,7 @@
+const frozen = new StatusEffect("frozen");
+  frozen.damageMultiplier = 0.000001;
+  frozen.speedMultiplier = 0.0000001;
+  frozen.armorMultiplier = 0.5;
+  frozen.damage = 0.4;
+  frozen.color = 36d7ff;
+  frozen.effect = smoke;
